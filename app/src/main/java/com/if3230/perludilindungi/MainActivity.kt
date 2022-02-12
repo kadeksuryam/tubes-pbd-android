2137657a27a3a8c40831c88268fc4e66d58c43f9
Josep Marcello <13519164@std.stei.itb.ac.id>
2022-02-12 22:49:06 +0700

    chore: init project with empty activity

---
+++ app/src/main/java/com/if3230/perludilindungi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.if3230.perludilindungi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
