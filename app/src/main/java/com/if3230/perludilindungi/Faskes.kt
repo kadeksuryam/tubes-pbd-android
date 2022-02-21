cb774839e8dc5f4e18d958e31ac0e0097bd0b386
Josep Marcello <13519164@std.stei.itb.ac.id>
2022-02-21 09:18:40 +0700

    fix: rename fragment FaskesSearch to Faskes

--- app/src/main/java/com/if3230/perludilindungi/Faskes.kt
+++ app/src/main/java/com/if3230/perludilindungi/Faskes.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FaskesSearch.newInstance] factory method to
+ * Use the [Faskes.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FaskesSearch : Fragment() {
+class Faskes : Fragment() {
 	// TODO: Rename and change types of parameters
 	private var param1: String? = null
 	private var param2: String? = null
@@ -34,7 +34,7 @@ class FaskesSearch : Fragment() {
 		savedInstanceState: Bundle?
 	): View? {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_faskes_search, container, false)
+		return inflater.inflate(R.layout.fragment_faskes, container, false)
 	}
 
 	companion object {
@@ -49,7 +49,7 @@ class FaskesSearch : Fragment() {
 		// TODO: Rename and change types and number of parameters
 		@JvmStatic
 		fun newInstance(param1: String, param2: String) =
-			FaskesSearch().apply {
+			Faskes().apply {
 				arguments = Bundle().apply {
 					putString(ARG_PARAM1, param1)
 					putString(ARG_PARAM2, param2)
