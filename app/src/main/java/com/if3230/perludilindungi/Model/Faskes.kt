a16eda37c3af0cc1d53552b06d8ab7ae40cb97b0
Josep Marcello <13519164@std.stei.itb.ac.id>
2022-03-03 09:15:43 +0700

    feat: add model for faskes

---
+++ app/src/main/java/com/if3230/perludilindungi/Model/Faskes.kt
@@ -0,0 +1,57 @@
+package com.if3230.perludilindungi.Model
+
+enum class JenisFaskes {
+	RUMAH_SAKIT {
+		override fun toString(): String {
+			return "RUMAH SAKIT"
+		}
+	},
+	NULL {
+		override fun toString(): String {
+			return ""
+		}
+	},
+	KLINIK,
+	SENTRA,
+	FKTP,
+	PUSKESMAS,
+}
+enum class StatusFaskes {
+	SIAP_VAKSINASI {
+		override fun toString(): String {
+			return "Siap Vaksinasi"
+		}
+	}
+}
+data class DetailFaskes(
+	val id: Int,
+	val kode: String,
+	val batch: String,
+	val divaksin: Int,
+	val divaksin_1: Int,
+	val divaksin_2: Int,
+	val batal_vaksin: Int,
+	val batal_vaksin_1: Int,
+	val batal_vaksin_2: Int,
+	val pending_vaksin: Int,
+	val pending_vaksin_1: Int,
+	val pending_vaksin_2: Int,
+	val tanggal: String,
+)
+
+data class Faskes(
+	val id: Int,
+	val kode: String,
+	val nama: String,
+	val kota: String,
+	val provinsi: String,
+	val alamat: String,
+	val latitude: String,
+	val longitude: String,
+	val telp: String,
+	val jenis_fakses: String,
+	val kelas_rs: String,
+	val status: String,
+	val detail: List<DetailFaskes>,
+	val source_data: String,
+)
---
+++ app/src/main/java/com/if3230/perludilindungi/Model/FaskesResponse.kt
@@ -0,0 +1,8 @@
+package com.if3230.perludilindungi.Model
+
+data class FaskesResponse(
+	val success: Boolean,
+	val message: String,
+	val count_total: Int,
+	val results: List<Faskes>
+)
