97cba4a1ebd12a44eef4e9939b2e123f1910c6fa
azmims <13519151@std.stei.itb.ac.id>
2022-03-03 09:53:37 +0700

    search faskes

--- app/src/main/java/com/if3230/perludilindungi/Faskes.kt
+++ app/src/main/java/com/if3230/perludilindungi/Faskes.kt
@@ -1,10 +1,14 @@
 package com.if3230.perludilindungi
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.ArrayAdapter
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.appbar.MaterialToolbar
+import com.if3230.perludilindungi.databinding.FragmentFaskesBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,12 +25,42 @@ class Faskes : Fragment() {
 	private var param1: String? = null
 	private var param2: String? = null
 
+//	private var _binding: Faskes? = null
+//	private val binding get() = _binding!!
+
+//	private val viewModel by lazy { ViewModelProvider(requireActivity()).get(ContentViewModel::class.java) }
+
+	private lateinit var binding: FragmentFaskesBinding
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		arguments?.let {
 			param1 = it.getString(ARG_PARAM1)
 			param2 = it.getString(ARG_PARAM2)
 		}
+
+	}
+
+//	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//		super.onViewCreated(view, savedInstanceState)
+//
+//		viewModel.updateActionBarTitle("Custom Title From Fragment")
+//	}
+
+	override fun onResume() {
+		super.onResume()
+
+		val provinsi = resources.getStringArray(R.array.provinsi)
+		var arrayAdapterProvinsi = ArrayAdapter(requireContext(), R.layout.dropdown_item, provinsi)
+		binding.autoCompleteTextView.setAdapter(arrayAdapterProvinsi)
+
+		val kota = resources.getStringArray(R.array.kota)
+		var arrayAdapterKota = ArrayAdapter(requireContext(), R.layout.dropdown_item, kota)
+		binding.autoCompleteTextView6.setAdapter(arrayAdapterKota)
+
+		val act = requireActivity()
+		val topBar: MaterialToolbar = act.findViewById(R.id.content_top_bar)!!
+		topBar.title = act.getString(R.string.title_faskes_search_fragment)
 	}
 
 	override fun onCreateView(
@@ -34,7 +68,10 @@ class Faskes : Fragment() {
 		savedInstanceState: Bundle?
 	): View? {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_faskes, container, false)
+		binding = FragmentFaskesBinding.inflate(inflater, container, false)
+
+
+		return binding.root
 	}
 
 	companion object {
